dcf3adc7e365c61b3b223b91b3407083876e6a6d
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-02-23 12:27:22 +0700

    Add Example number and remove comment

--- Enter_Update_Exit.js
+++ Enter_Update_Exit.js
@@ -2,7 +2,8 @@
 // DOM Element > data element (Update)
 // DOM Element = data element (Exit)
 
-/*
+
+//-------------------------------Example 1-----------------------------------------
 
 const data = [10]
 const canvas = d3.select("body").append("svg").attr("width",500).attr("height",500)
@@ -25,11 +26,11 @@ const circles = canvas.selectAll("circle").data(data)
 // เมื่อมีการ selectAll("circle") มันจะไม่ว่างอีกต่อไปเนื่องจากไป select circle มา.
 // และสิ่งที่เกิดขึ้นต่อมาคือ .enter() จะไม่สร้างวงกลมเพิ่มเนื่องจาก วงกลมแรกสร้างไปแล้ว คือ circle
 
-*/
 
-//---------------------------------------------------------------------------------
 
-/*
+//-------------------------------Example 2-----------------------------------------
+
+
 
 const data = [10,20]
 const canvas = d3.select("body").append("svg").attr("width",500).attr("height",500)
@@ -51,11 +52,11 @@ const circles = canvas.selectAll("circle").data(data).attr("fill","red")
 
 // data ตัวแรกจะทำงานใน circle และ data ตัวที่สองจะทำงานใน .enter()
 
-*/
 
-//-------------------------------------------------------------------------------
 
-/*
+//-------------------------------Example 3-----------------------------------------
+
+
 const data = [10,20]
 const canvas = d3.select("body").append("svg").attr("width",500).attr("height",500)
 
@@ -79,9 +80,9 @@ const circles = canvas.selectAll("circle").data(data).attr("fill","red")
 
 // data ตัวแรกจะทำงานใน circle1 และ data ตัวที่สองจะทำงานใน circle2 และ .enter() จะว่าง
 
-*/
 
-//-------------------------------------------------------------------------------
+
+//-------------------------------Example 4-----------------------------------------
 
 
 const data = [10]
